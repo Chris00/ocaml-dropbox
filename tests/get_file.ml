3174d30f245e72772100356167ce86f088a71b9e
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2015-01-03 14:03:07 +0100

    get_file (test): print some information on the file

--- tests/get_file.ml
+++ tests/get_file.ml
@@ -7,19 +7,23 @@ let download t fn =
   | Some(m, stream) ->
      (* Save stream to the disk *)
      let fname = Filename.basename fn in
-     let fd0 = Unix.openfile fname [Unix.O_WRONLY; Unix.O_CREAT] 0o664 in
-     let fd = Lwt_unix.of_unix_file_descr fd0 in
+     Lwt_unix.(openfile fname [O_WRONLY; O_CREAT; O_TRUNC] 0o664)
+     >>= fun fd ->
      let write s =
        Lwt_unix.write fd s 0 (String.length s) >>= fun _ ->
        return_unit in
-     Lwt_stream.iter_s write stream >>= fun _ ->
-     Unix.close fd0;
-     return_unit
+     Lwt_stream.iter_s write stream >>= fun () ->
+     Lwt_unix.close fd >>= fun () ->
+     Lwt_io.printlf "Wrote %S (%s, %s), rev: %s"
+                    fn m.D.size m.D.mime_type m.D.rev
 
 let main t args =
   match args with
   | [] -> Lwt_io.printlf "No file specified."
-  | _ -> Lwt_list.iter_p (download t) args
+  | _ ->
+     (* You can replace Lwt_list.iter_p by Lwt_list.iter_s for a
+        sequential download and see how much slower it is. *)
+     Lwt_list.iter_p (download t) args
 
 let () =
   Common.run main
