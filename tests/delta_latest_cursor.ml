ea13006a2cc4d3ef36d311f0af7744f7a95d8d3c
Alexandre Gravy <alex_gravy@hotmail.com>
2015-03-16 09:32:55 +0100

    Add doc

--- tests/delta_latest_cursor.ml
+++
@@ -1,9 +0,0 @@
-open Lwt
-module D = Dropbox_lwt_unix
-
-let main t args =
-  D.delta_latest_cursor t >>= fun delta ->
-  Lwt_io.printlf "%s" (Dropbox_j.string_of_delta delta)
-	    
-let () =
-  Common.run main
---
+++ tests/latest_cursor.ml
@@ -0,0 +1,20 @@
+open Lwt
+module D = Dropbox_lwt_unix
+
+(** If there is no entry, we call latest_cursor without param.
+    If there is one entry, we call latest_cursor with the param
+       path_prefix as Sys.argv.(0). *)
+
+
+
+let main t args =
+  match args with
+  | [] ->   D.delta t >>= fun delta ->
+            Lwt_io.printlf "%s" (Dropbox_j.string_of_delta delta)
+  | a -> if List.length a = 1 then
+           D.latest_cursor ~path_prefix:(List.hd a) t >>= fun delta ->
+           Lwt_io.printlf "%s" (Dropbox_j.string_of_delta delta)
+         else Lwt_io.printf ("The function must take the arguments like above")
+	    
+let () =
+  Common.run main
