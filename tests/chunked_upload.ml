6b248f94b51fe4a8bf11df50b7f4dbb6f044c070
Alexandre Gravy <alex_gravy@hotmail.com>
2015-03-19 00:46:05 +0100

    Chunk_upload who send chunks using rec function

--- tests/chunked_upload.ml
+++ tests/chunked_upload.ml
@@ -1,70 +1,62 @@
 open Lwt
 module D = Dropbox_lwt_unix
 
-let create_list i =
-  let list = ref [] in
-  for j = 0 to i do
-    list := abs(j - i) :: !list;
-  done;
-  !list ;;
+exception Wrong_offset of string
 
 let upload t fn =
   (** We first open the file and get its size *)
-  Lwt_unix.(openfile fn [O_RDONLY] 0) >>= fun fd -> Lwt_unix.stat fn
-  >>= fun u -> return(u.Lwt_unix.st_size) >>= fun size ->
+  Lwt_unix.(openfile fn [O_RDONLY] 0)
+  >>= fun fd -> Lwt_unix.stat fn >>= fun u ->
+  let size = u.Lwt_unix.st_size in
   let lgth_buff = 4194304 in
-  (** If the size of the file is < 4Mo, we use cohttp_body_files_put function,
-      otherwise chunked_upload function *)
+  (** If the size of the file is < 4Mo, we use cohttp_body_files_put
+      function. Otherwise chunked_upload function *)
   if size < lgth_buff then
     let buffer = String.create size in
     Lwt_unix.read fd buffer 0 size >>= fun _ ->
     let stream = Cohttp_lwt_body.of_string buffer in
     D.cohttp_body_files_put t fn size stream
-    >>= fun m -> Lwt_unix.close fd
-    >>= fun () -> Lwt_io.printlf "Sended: %s\nMetadata:\n%s"
-                  fn (Dropbox_j.string_of_metadata m)
+    >>= fun metadata -> Lwt_unix.close fd
+    >>= fun () -> Lwt_io.printlf "Send: %s\nMetadata:\n%s"
+                  fn (Dropbox_j.string_of_metadata metadata)
   else (** The size is > 4Mo *)
     let numb = size / lgth_buff in
     let buffer = String.create lgth_buff in
-    let upload_id = ref "" in
-    (** We use the serialization of Lwt_list.iter_s to create our chunk
-        upload (require a list, so we create a list [0;1;2;3;...;numb]
-        When s = 0, we request the first chunk of the file without setting
-        upload_id, and receive an upload_id. Until num = numb, we request
-        each following chunk by parameting the offset and the upload_id.
-        Then (num = numb), we request the last chunk whos size is < lgt_buff *)
-        let read s = match s with
-      | 0 -> Lwt_unix.read fd buffer 0 lgth_buff >>= fun _ ->
-             let stream = Cohttp_lwt_body.of_string buffer in
-             D.chunked_upload t stream >>= fun chunked_upload ->
-             upload_id := (chunked_upload.D.upload_id);
-             Lwt_io.printlf "\nUpload_id: %s" (!upload_id)
-             >>= fun () -> return ()
-      | num -> if num = numb then
-                 Lwt_io.printlf "Offset: %i \nEnd of file" (num * lgth_buff)
-                 >>= fun () -> Lwt_unix.read fd buffer 0 lgth_buff
-                 >>= fun size ->
-                 let stream = Cohttp_lwt_body.of_string
-                   (String.sub buffer 0 size) in
-                 D.chunked_upload t ~upload_id:(!upload_id)
-                   ~offset:(num * lgth_buff) stream
-                 >>= fun chunked_upload -> return ()
-               else
-                 Lwt_io.printlf "Offset: %i" (num * lgth_buff)
-                 >>= fun () -> Lwt_unix.read fd buffer 0 lgth_buff
-                 >>= fun _ ->
-                 let stream = Cohttp_lwt_body.of_string buffer in
-                 D.chunked_upload t ~upload_id:(!upload_id)
-                   ~offset:(num * lgth_buff) stream
-                 >>= fun _ -> return()
-        in
+    let rec send_chunks ?(upload_id="") ?(offset=0) lgth =
+      (** To see if the offset = the number of bytes actually read
+          by the function. If not, raise exception Wrong_offset *)
+      if offset = ((numb - lgth) * lgth_buff) then
+        (** Initializing with upload_id *)
+        if lgth = numb then
+          Lwt_unix.read fd buffer 0 lgth_buff >>= fun _ ->
+          let stream = Cohttp_lwt_body.of_string buffer in
+          D.chunked_upload t stream >>= fun chkd_upld ->
+          Lwt_io.printlf "Upload_id: %s" (chkd_upld.D.upload_id)
+          >>= fun () -> send_chunks ~upload_id:(chkd_upld.D.upload_id)
+                        ~offset:(chkd_upld.D.offset) (lgth-1)
+        else if lgth = 0 then
+          Lwt_io.printlf "Offset: %i \nEnd of file" offset
+          >>= fun () -> Lwt_unix.read fd buffer 0 lgth_buff >>= fun size ->
+          let stream = Cohttp_lwt_body.of_string (String.sub buffer 0 size) in
+          D.chunked_upload t ~upload_id ~offset stream
+          >>= fun _ -> return(upload_id)
+        else
+          Lwt_io.printlf "Offset: %i" offset
+          >>= fun () -> Lwt_unix.read fd buffer 0 lgth_buff >>= fun _ ->
+          let stream = Cohttp_lwt_body.of_string buffer in
+          D.chunked_upload t ~upload_id ~offset stream
+          >>= fun chkd_upld -> send_chunks ~upload_id
+                               ~offset:(chkd_upld.D.offset) (lgth-1)
+      else
+        let msg = "Last offset: " ^ (string_of_int offset) in
+        fail(Wrong_offset msg)
+    in
     (** Finally, we finish the upload of the file by using
         commit_chunked_upload *)
-    Lwt_list.iter_s read (create_list numb)
-    >>= fun () -> D.commit_chunked_upload t (!upload_id) fn
-    >>= fun m -> Lwt_unix.close fd
-    >>= fun () -> Lwt_io.printlf "Sended: %s\nMetadata:\n%s"
-                  fn (Dropbox_j.string_of_metadata m)
+    send_chunks numb >>= fun upload_id -> D.commit_chunked_upload t upload_id fn
+    >>= fun metadata -> Lwt_unix.close fd
+    >>= fun () -> Lwt_io.printlf "Send: %s\nMetadata:\n%s"
+                  fn (Dropbox_j.string_of_metadata metadata)
 
 let main t args =
   match args with
