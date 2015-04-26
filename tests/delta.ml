61bf726561db41a32c804bfa77c9b43c6706c7df
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2015-04-27 00:58:46 +0200

    Use a cursor abstraction so it cannot be misused.
    Fix "delta" and "latest_cursor" accordingly.

--- tests/delta.ml
+++ tests/delta.ml
@@ -7,20 +7,28 @@ module D = Dropbox_lwt_unix
     If there is two entries, we call delta with the param cursor as
        Sys.argv.(0) and the param path_prefix as the second. *)
 
+let print_delta delta =
+  let entries = String.concat ", " (List.map fst delta.D.entries) in
+  Lwt_io.printlf "Entries: %s\nReset: %b\nhas_more: %b\n"
+                 entries delta.D.reset delta.D.has_more
 
+let rec print_deltas ?path_prefix t =
+  D.delta t ?path_prefix >>= fun delta ->
+  print_delta delta >>= fun () ->
+  if delta.D.has_more then print_deltas_cursor t delta.D.cursor
+  else return()
+
+and print_deltas_cursor t cursor =
+  D.delta t ~cursor >>= fun delta ->
+  print_delta delta >>= fun () ->
+  if delta.D.has_more then print_deltas_cursor t delta.D.cursor
+  else return ()
 
 let main t args =
   match args with
-  | [] ->   D.delta t >>= fun delta ->
-            Lwt_io.printlf "%s" (Dropbox_j.string_of_delta delta)
-  | [path_prefix] ->
-     D.delta t ~path_prefix >>= fun delta ->
-     Lwt_io.printlf "%s" (Dropbox_j.string_of_delta delta)
-  | [path_prefix; cursor] ->
-     D.delta t ~cursor ~path_prefix >>= fun delta ->
-     Lwt_io.printlf "%s" (Dropbox_j.string_of_delta delta)
-  | _ ->
-     Lwt_io.printf "Usage: delta [path_prefix [delta]]"
+  | [] -> print_deltas t
+  | [path_prefix] -> print_deltas t ~path_prefix
+  | _ -> Lwt_io.printlf "Usage: delta [path_prefix]"
 
 let () =
   Common.run main
--- tests/latest_cursor.ml
+++ tests/latest_cursor.ml
@@ -1,15 +1,20 @@
 open Lwt
 module D = Dropbox_lwt_unix
 
+let print_delta delta =
+  let entries = String.concat ", " (List.map fst delta.D.entries) in
+  Lwt_io.printlf "Entries: %s\nReset: %b\nhas_more: %b\n"
+                 entries delta.D.reset delta.D.has_more
 
 let main t args =
   match args with
-  | [] -> D.delta t >>= fun delta ->
-          Lwt_io.printlf "%s" (Dropbox_j.string_of_delta delta)
-  | [path] ->
-     D.latest_cursor ~path_prefix:path t >>= fun delta ->
-     Lwt_io.printlf "%s" (Dropbox_j.string_of_delta delta)
-  | _ -> Lwt_io.printf "The function must take 0 or 1 argument"
+  | [] -> D.latest_cursor t >>= fun cursor ->
+          D.delta t ~cursor >>= fun delta ->
+          print_delta delta
+  | [path] -> D.latest_cursor ~path_prefix:path t >>= fun cursor ->
+              D.delta t ~cursor >>= fun delta ->
+              print_delta delta
+  | _ -> Lwt_io.printlf "Usage: latest_cursor [path]"
 
 let () =
   Common.run main
