27d8fff02fbf65c4d1dbffb822afdb78676678ac
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2015-04-26 18:24:46 +0200

    Use pattern-matching instead of "if" for 1-element list

--- tests/latest_cursor.ml
+++ tests/latest_cursor.ml
@@ -1,20 +1,15 @@
 open Lwt
 module D = Dropbox_lwt_unix
 
-(** If there is no entry, we call latest_cursor without param.
-    If there is one entry, we call latest_cursor with the param
-       path_prefix as Sys.argv.(0). *)
-
-
 
 let main t args =
   match args with
-  | [] ->   D.delta t >>= fun delta ->
-            Lwt_io.printlf "%s" (Dropbox_j.string_of_delta delta)
-  | a -> if List.length a = 1 then
-           D.latest_cursor ~path_prefix:(List.hd a) t >>= fun delta ->
-           Lwt_io.printlf "%s" (Dropbox_j.string_of_delta delta)
-         else Lwt_io.printf "The function must take the arguments like above"
+  | [] -> D.delta t >>= fun delta ->
+          Lwt_io.printlf "%s" (Dropbox_j.string_of_delta delta)
+  | [path] ->
+     D.latest_cursor ~path_prefix:path t >>= fun delta ->
+     Lwt_io.printlf "%s" (Dropbox_j.string_of_delta delta)
+  | _ -> Lwt_io.printf "The function must take 0 or 1 argument"
 
 let () =
   Common.run main
