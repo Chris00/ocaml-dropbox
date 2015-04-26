479ae1c839bb6d74603ebbcf4b3bb2534b808180
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2015-04-27 01:18:48 +0200

    Let "longpoll_delta" use the "cursor" abstraction

--- tests/longpoll_delta.ml
+++ tests/longpoll_delta.ml
@@ -1,14 +1,10 @@
 open Lwt
 module D = Dropbox_lwt_unix
 
-let longpoll t cursor =
-  D.longpoll_delta t cursor >>= fun delta ->
+let main t args =
+  D.delta t >>= fun delta ->
+  D.longpoll_delta t delta.D.cursor >>= fun delta ->
   Lwt_io.printlf "%s" (Dropbox_j.string_of_longpoll_delta delta)
 
-let main t args =
-  match args with
-  | [] -> Lwt_io.printlf "No cursor specified"
-  | _ -> Lwt_list.iter_p (longpoll t) args
-	    
 let () =
   Common.run main
