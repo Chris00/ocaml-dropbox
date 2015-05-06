a5232d55c4722af1031b7b1ddef79d83907aeba4
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2015-05-06 23:49:49 +0200

    Test revisions: print the date if available

--- tests/revisions.ml
+++ tests/revisions.ml
@@ -1,17 +1,22 @@
+open Printf
 open Lwt
 module D = Dropbox_lwt_unix
 
 let revisions t fn =
   D.revisions t fn >>= function
   | Some metadata_list ->
-    let get_rev metadata = metadata.D.rev in
-    let revisions = String.concat ", rev: " (List.map get_rev metadata_list) in
-    Lwt_io.printlf "rev: %s" revisions
-  | None -> Lwt_io.printlf "No file %s" fn
+     let show_rev metadata =
+       let d = match metadata.D.modified with
+         | Some d -> sprintf " (%s)" (Dropbox.Date.to_string d)
+         | None -> "" in
+       sprintf "rev: %s%s" metadata.D.rev d in
+    let revisions = String.concat "\n" (List.map show_rev metadata_list) in
+    Lwt_io.printlf "%s" revisions
+  | None -> Lwt_io.printlf "No file %S." fn
 
 let main t args =
   match args with
-  | [] -> Lwt_io.printlf "No file or folder specified"
+  | [] -> Lwt_io.printlf "No file specified"
   | _ -> Lwt_list.iter_p (revisions t) args
 
 let () =
