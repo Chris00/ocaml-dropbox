9bf2b58e255fad332c66a9da34ec5ccbb74c831f
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2015-05-07 00:27:56 +0200

    Test search: persent 1 result per line

--- tests/search.ml
+++ tests/search.ml
@@ -7,19 +7,17 @@ module D = Dropbox_lwt_unix
 
 let print_search meta_list query =
   match List.length meta_list with
-  | 0 -> Lwt_io.printlf "No file or folder whose name contains %s found." query
+  | 0 -> Lwt_io.printlf "No file or folder whose name contains %S found." query
   | _ -> let get_path m = m.D.path in
-         let paths = String.concat ", " (List.map get_path meta_list) in
+         let paths = String.concat "\n" (List.map get_path meta_list) in
          Lwt_io.printlf "%s" paths
 
 let main t args =
   match args with
-  | [] -> Lwt_io.printlf "No file or folder specified"
   | [query] -> D.search t query >>= fun search -> print_search search query
-  | [fn;query] -> D.search t ~fn query
+  | [query; fn] -> D.search t ~fn query
                   >>= fun search -> print_search search query
-  | _ -> Lwt_io.printf "Error, the function must take on command line only \
-                        one or two arguments the query and optionnaly the path."
+  | _ -> Lwt_io.printf "%s <query> [path]\n" Sys.argv.(0)
 
 let () =
   Common.run main
