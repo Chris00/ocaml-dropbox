b7cf96b0bc80c344dfa327d0e57afc59c8929032
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2015-05-17 16:47:31 +0200

    Report the actual name of the file written by the "previews" test

--- tests/previews.ml
+++ tests/previews.ml
@@ -18,7 +18,7 @@ let download t fn =
       return_unit in
     Lwt_stream.iter_s write stream >>= fun () ->
     Lwt_unix.close fd >>= fun () ->
-    Lwt_io.printlf "Wrote %s" fn
+    Lwt_io.printlf "Wrote \"%s\"." fname
 
 let main t args =
   match args with
